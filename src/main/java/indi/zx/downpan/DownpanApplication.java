30ab944edfcf25ed68571a3ecada26e71b9517cb
Your Name <384027864@qq.com>
2021-03-08 23:48:36 +0800

    --增加socket.io--

--- src/main/java/indi/zx/downpan/DownpanApplication.java
+++ src/main/java/indi/zx/downpan/DownpanApplication.java
@@ -1,18 +1,21 @@
 package indi.zx.downpan;
 
+import ch.qos.logback.core.net.server.ServerRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
 @SpringBootApplication
-public class DownpanApplication {
+public class DownpanApplication  {
+
 
     public static void main(String[] args) {
         SpringApplication.run(DownpanApplication.class, args);
-    }
 
+    }
 }
